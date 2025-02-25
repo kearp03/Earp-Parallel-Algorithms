d4361d83d16554ee0552fd3a2d332e3862d8773b
Kyle Earp <131403485+kearp03@users.noreply.github.com>
2025-02-24 18:35:26 -0600

    Update HW10.cu
    Speed up dot product by enforcing certain restrictions on input

--- HW10/HW10.cu
+++ HW10/HW10.cu
@@ -16,9 +16,9 @@
     but your client is using an old GPU. Check both and if either is out of bound report it to your client then kindly
     exit the program.
  3. Always checking to see if you have threads working past your vector is a real pain and adds a bunch of time consumming
-    if statments to your GPU code. To get around this findout how much you would have to add to your vector to make it 
+    if statments to your GPU code. To get around this find out how much you would have to add to your vector to make it 
     perfectly fit in your block and grid layout and pad it with zeros. Multipying zeros and adding zero do nothing to a 
-    dot product. If you were luck on HW8 you kind of did this but you just got lucky because most of the time the GPU sets
+    dot product. If you were lucky on HW8 you kind of did this but you just got lucky because most of the time the GPU sets
     everything to zero at start up. But!!!, you don't want to put code out where you are just lucky soooo do a cudaMemset
     so you know everything is zero. Then copy up the now zero values.
  4. In HW9 we had to do the final add "reduction' on the CPU because we can't sync block. Use atomic add to get around 
@@ -34,8 +34,8 @@
 #include <stdio.h>
 
 // Defines
-#define N 100000 // Length of the vector
-#define BLOCK_SIZE 200 // Threads in a block
+#define N 1'000'000 // Length of the vector
+#define BLOCK_SIZE 512 // Threads in a block
 
 // Global variables
 float *A_CPU, *B_CPU, *C_CPU; //CPU pointers
@@ -80,6 +80,69 @@ void setUpDevices()
 	GridSize.x = (N - 1)/BlockSize.x + 1; // This gives us the correct number of blocks.
 	GridSize.y = 1;
 	GridSize.z = 1;
+
+	// Making sure the number of threads in a block is a power of 2.
+	if((int)(BlockSize.x & (BlockSize.x - 1)) != 0)
+	{
+		printf("\n\n The number of threads in a block must be a power of 2\n");
+		exit(0);
+	}
+
+	// Picking the best GPU to use based on the compute capability.
+	int count;
+	int best_idx = 0;
+	cudaGetDeviceCount(&count);
+	cudaErrorCheck(__FILE__, __LINE__);
+	cudaDeviceProp props[count];
+	if(count == 0)
+	{
+		printf("\n\n There are no GPUs on this machine\n");
+		exit(0);
+	}
+	else
+	{
+		cudaGetDeviceProperties(&props[0], 0);
+		cudaErrorCheck(__FILE__, __LINE__);
+	}
+	for(int i = 1; i < count; i++)
+	{
+		cudaGetDeviceProperties(&props[i], i);
+		cudaErrorCheck(__FILE__, __LINE__);
+		if(props[i].major >= 3 && props[i].major > props[best_idx].major)
+		{
+			best_idx = i;
+		}
+		if(props[i].major == props[best_idx].major)
+		{
+			if(props[i].minor > props[best_idx].minor)
+			{
+				best_idx = i;
+			}
+		}
+	}
+	// For the case when none of the GPUs do not have the correct compute capability.
+	if(props[best_idx].major < 3)
+	{
+		printf("\n\n The GPU does not have the correct compute capability: %d.%d\n", props[best_idx].major, props[best_idx].minor);
+		exit(0);
+	}
+
+	// Setting the device to the best GPU.
+	cudaSetDevice(best_idx);
+	cudaErrorCheck(__FILE__, __LINE__);
+
+	// Making sure the number of threads in a block and the number of blocks in the grid are not out of bounds.
+	if(BlockSize.x <= 0 || BlockSize.x > props[best_idx].maxThreadsPerBlock)
+	{
+		printf("\n\n The number of threads in a block is out of bounds: (%d, %d, %d)\n", BlockSize.x, BlockSize.y, BlockSize.z);
+		exit(0);
+	}
+
+	if(GridSize.x <= 0 || GridSize.x > props[best_idx].maxGridSize[0])
+	{
+		printf("\n\n The number of blocks in the grid is out of bounds: (%d, %d, %d)\n", GridSize.x, GridSize.y, GridSize.z);
+		exit(0);
+	}
 }
 
 // Allocating the memory we will be using.
@@ -91,11 +154,11 @@ void allocateMemory()
 	C_CPU = (float*)malloc(N*sizeof(float));
 	
 	// Device "GPU" Memory
-	cudaMalloc(&A_GPU,N*sizeof(float));
+	cudaMalloc(&A_GPU,BlockSize.x*GridSize.x*sizeof(float));
 	cudaErrorCheck(__FILE__, __LINE__);
-	cudaMalloc(&B_GPU,N*sizeof(float));
+	cudaMalloc(&B_GPU,BlockSize.x*GridSize.x*sizeof(float));
 	cudaErrorCheck(__FILE__, __LINE__);
-	cudaMalloc(&C_GPU,N*sizeof(float));
+	cudaMalloc(&C_GPU,sizeof(float));
 	cudaErrorCheck(__FILE__, __LINE__);
 }
 
@@ -130,31 +193,26 @@ __global__ void dotProductGPU(float *a, float *b, float *c, int n)
 	int threadIndex = threadIdx.x;
 	int vectorIndex = threadIdx.x + blockDim.x*blockIdx.x;
 	__shared__ float c_sh[BLOCK_SIZE];
-	
+
 	c_sh[threadIndex] = (a[vectorIndex] * b[vectorIndex]);
 	__syncthreads();
 	
 	int fold = blockDim.x;
 	while(1 < fold)
 	{
-		if(fold%2 != 0)
-		{
-			if(threadIndex == 0 && (vectorIndex + fold - 1) < n)
-			{
-				c_sh[0] = c_sh[0] + c_sh[0 + fold - 1];
-			}
-			fold = fold - 1;
-		}
 		fold = fold/2;
-		if(threadIndex < fold && (vectorIndex + fold) < n)
+		if(threadIndex < fold)
 		{
-			c_sh[threadIndex] = c_sh[threadIndex] + c_sh[threadIndex + fold];
+			c_sh[threadIndex] += c_sh[threadIndex + fold];
 			
 		}
 		__syncthreads();
 	}
 	
-	c[blockDim.x*blockIdx.x] = c_sh[0];
+	if(threadIndex == 0)
+	{
+		atomicAdd(c, c_sh[0]);
+	}
 }
 
 // Checking to see if anything went wrong in the vector addition.
@@ -229,28 +287,32 @@ int main()
 	// Adding on the GPU
 	gettimeofday(&start, NULL);
 	
-	// Copy Memory from CPU to GPU		
+	// Making sure the GPU is starting with a clean slate.
+	cudaMemset(A_GPU, 0, BlockSize.x*GridSize.x*sizeof(float));
+	cudaErrorCheck(__FILE__, __LINE__);
+	cudaMemset(B_GPU, 0, BlockSize.x*GridSize.x*sizeof(float));
+	cudaErrorCheck(__FILE__, __LINE__);
+	cudaMemset(C_GPU, 0, sizeof(float));
+	cudaErrorCheck(__FILE__, __LINE__);
+
+	// Copy Memory from CPU to GPU
 	cudaMemcpyAsync(A_GPU, A_CPU, N*sizeof(float), cudaMemcpyHostToDevice);
 	cudaErrorCheck(__FILE__, __LINE__);
 	cudaMemcpyAsync(B_GPU, B_CPU, N*sizeof(float), cudaMemcpyHostToDevice);
 	cudaErrorCheck(__FILE__, __LINE__);
-	
+
 	dotProductGPU<<<GridSize,BlockSize>>>(A_GPU, B_GPU, C_GPU, N);
 	cudaErrorCheck(__FILE__, __LINE__);
 	
 	// Copy Memory from GPU to CPU	
-	cudaMemcpyAsync(C_CPU, C_GPU, N*sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpyAsync(C_CPU, C_GPU, 1*sizeof(float), cudaMemcpyDeviceToHost);
 	cudaErrorCheck(__FILE__, __LINE__);
 	
 	// Making sure the GPU and CPU wiat until each other are at the same place.
 	cudaDeviceSynchronize();
 	cudaErrorCheck(__FILE__, __LINE__);
 	
-	DotGPU = 0.0;
-	for(int i = 0; i < N; i += BlockSize.x)
-	{
-		DotGPU += C_CPU[i]; // C_GPU was copied into C_CPU. 
-	}
+	DotGPU = C_CPU[0];
 
 	gettimeofday(&end, NULL);
 	timeGPU = elaspedTime(start, end);
@@ -267,7 +329,7 @@ int main()
 		printf("\n The time it took on the GPU was %ld microseconds", timeGPU);
 	}
 	
-	// Your done so cleanup your room.	
+	// You're done so cleanup your room.	
 	cleanUp();	
 	
 	// Making sure it flushes out anything in the print buffer.
@@ -275,5 +337,3 @@ int main()
 	
 	return(0);
 }
-
-
