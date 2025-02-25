ad2ceddc4236e77d2d9b6d282c00a435869bd50e
Kyle Earp <131403485+kearp03@users.noreply.github.com>
2025-02-25 11:05:24 -0600

    Create HW11.cu
    Upload HW11

---
+++ HW11/HW11.cu
@@ -0,0 +1,8 @@
+// Name: Kyle Earp
+// Keep working on HW10 and your TORUS presentations
+// nvcc TORUS.cu -o temp
+/*
+ What to do:
+ Keep polishing your HW10 and give a good presentation at TORUS
+*/
+
