15298e458a2b99ebd08bc56543daafde108d38bf
Kyle Earp <131403485+kearp03@users.noreply.github.com>
2025-04-24 18:34:13 -0500

    Update HW27.cu
    Create a function to demonstrate how a random walk is not always consistent

--- HW27/HW27.cu
+++ HW27/HW27.cu
@@ -10,23 +10,43 @@
 */
 
 // Include files
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Defines
-
+// None
 
 // Globals
-
+int NumberOfSteps = 10'000; // Number of steps in the random walk
 
 // Function prototypes
-
-
+int randomWalk();
 int main(int, char**);
 
+int randomWalk()
+{
+	// Randomly return -1 or 1
+	return (rand() % 2 == 0) ? -1 : 1;
+}
 
 int main(int argc, char** argv)
 {
+	// Seed the random number generator
+	srand(time(NULL));
+
+	// Initialize the position
+	int position = 0;
+
+	// Loop through the number of steps
+	for(int i = 0; i < NumberOfSteps; i++)
+	{
+		// Call the randomWalk function to get a step
+		position += randomWalk();
+	}
+
+	// Print the final position
+	printf("Final position: %d\n", position);
 	
 	return 0;
 }
-
